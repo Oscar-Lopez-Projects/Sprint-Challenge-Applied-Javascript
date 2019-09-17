33b033c4e7202ae9aa7dc386ab9d78fd638ec9ba
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2019-09-16 17:56:03 -0700

    finished mvp

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,11 +2,32 @@
 // -----------------------
 // Using a function create the component you see below:
 //
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
+//  <div class="header">(done)
+//    <span class="date">SMARCH 28, 2019</span>(done)
+//    <h1>Lambda Times</h1>(done)
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(head) {
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("header");
+    
+    const date= document.createElement("span");
+    date.classList.add("date");
+    date.textContent= 'SMARCH 28, 2019';
+    headerDiv.appendChild(date);
+    
+    const h1 = document.createElement("h1");
+  h1.textContent = 'Lambda Times';
+  headerDiv.appendChild(h1);
+
+  const temp = document.createElement("span");
+  temp.classList.add("temp");
+  temp.textContent = '98';
+  headerDiv.appendChild(temp);
+    return headerDiv;
+}
+
+const mainHead = document.querySelector('.header-container');
+mainHead.appendChild(Header());
